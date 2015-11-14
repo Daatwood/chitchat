a0d325d6fab3f7aabdf593f3e08c6f78e64369d5
ItBones <atwood.dustin@gmail.com>
2015-11-14 13:24:55 -0600

    Version bump to 2.0
    Signed-off-by: ItBones <atwood.dustin@gmail.com>

--- deploy-release.bat
+++ deploy-release.bat
@@ -3,4 +3,4 @@ set /p build_path=< .build_path
 git rev-list HEAD --count > .commit
 set /p version=< .commit
 set /p wow_version=< .wow_version
-7za a %build_path%\%filename%\%filename%_%wow_version%-%version%.zip %filename%
+7za a %build_path%\%filename%\%filename%_%wow_version%-2.%version%.zip %filename%
